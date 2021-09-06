157c15443b4d13d7f944faaf818970ade1f7b314
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-09-06 21:02:07 +0100

    GET all posts from api and authorize with token and render each post after authorisation

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,7 +1,47 @@
-const Posts = () => {
+import { useEffect, useState } from "react"
+
+const Posts = ({ setUserAuthorised }) => {
+    const [allPosts, setAllPosts] = useState([])
+    const API_URL = process.env.REACT_APP_API_URL;
+
+    useEffect(() => {
+        const getAllPosts = async () => {
+            try {
+                const response = await fetch(`${API_URL}/posts`, {
+                    method: 'GET',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${sessionStorage.getItem('token')}`
+                    },
+                })
+                if (response.status === 401) {
+                    sessionStorage.removeItem('token')
+                    sessionStorage.removeItem('userAuth')
+                    setUserAuthorised(false)
+                }
+                const data = await response.json()
+                setAllPosts(data.all_posts)
+            } catch (err) {
+                console.error(err)
+            }
+        }
+        getAllPosts()
+    }, [API_URL, setUserAuthorised])
+
     return (
         <div>
-            Posts Page
+            {allPosts.map((post) => {
+                return (
+                    <div key={post._id}>
+                        <p>{post.author}</p>
+                        <p>{post.title}</p>
+                        <p>{post.text}</p>
+                        <p>{post.date}</p>
+                        <button>Edit</button>
+                    </div>
+                )
+            })}
         </div>
     )
 }
