6d6233aac452e060c304ee9a9a4a70808a00abe9
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-09-06 12:44:49 +0100

    added react-router-dom and setup routes

--- src/App.js
+++ src/App.js
@@ -2,16 +2,32 @@ import Login from './components/Login'
 import Posts from './components/Posts'
 import EditPost from './components/EditPost'
 import NewPost from './components/NewPost'
+import { BrowserRouter, Route, Switch } from "react-router-dom"
 
 function App() {
   return (
-    <div>
-      Blog CMS
-      <Login />
-      <Posts />
-      <EditPost />
-      <NewPost />
-    </div >
+    <BrowserRouter>
+      <h1>
+        Blog CMS
+      </h1>
+      <Switch>
+        <Route exact path='/login'>
+          <Login />
+        </Route>
+
+        <Route exact path='/posts'>
+          <Posts />
+        </Route>
+
+        <Route exact path='/edit-post'>
+          <EditPost />
+        </Route>
+
+        <Route exact path='/new-post'>
+          <NewPost />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
