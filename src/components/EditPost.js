4d35ed1eb4bccaf503a7a0ef120f1635ee409dc9
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-09-07 13:15:42 +0100

    GET post from db that is required to be edited and render in /posts/:id page

--- src/components/EditPost.js
+++ src/components/EditPost.js
@@ -1,7 +1,46 @@
-const EditPost = () => {
+import { useState, useEffect } from "react";
+import { useParams } from "react-router";
+
+const EditPost = ({ setUserAuthorised }) => {
+    const [post, setPost] = useState([]);
+    const API_URL = process.env.REACT_APP_API_URL;
+    const { id } = useParams();
+
+    useEffect(() => {
+        const getPost = async () => {
+            try {
+                const response = await fetch(`${API_URL}/posts/${id}`, {
+                    method: 'GET',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${sessionStorage.getItem('token')}`
+                    },
+                })
+                if (response.status === 401) {
+                    sessionStorage.removeItem('token')
+                    sessionStorage.removeItem('userAuth')
+                    setUserAuthorised(false)
+                }
+                const data = await response.json()
+                setPost(data.found_post)
+            } catch (err) {
+                console.error(err)
+            }
+        }
+        getPost()
+    }, [API_URL, id, setUserAuthorised])
+
+
+
     return (
         <div>
-            Edit Post Page
+            <div key={post._id}>
+                <p>{post.author}</p>
+                <p>{post.title}</p>
+                <p>{post.text}</p>
+                <p>{post.date}</p>
+            </div>
         </div>
     )
 }
